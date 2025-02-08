501f8f93099b15bd86ad8a8b9509547549cf4100
d1rtyloudx <ilia.mesha166@gmail.com>
2025-02-08 17:27:18 +0300

    add redis client conn

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 )
 
 require (
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/goccy/go-json v0.10.4 // indirect
@@ -18,6 +20,7 @@ require (
 	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/redis/go-redis/v9 v9.7.0 // indirect
 	github.com/rs/xid v1.6.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
--- redis/redis.go
+++ redis/redis.go
@@ -1,4 +1,40 @@
 package redis
 
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+)
+
 type Config struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+	DB       int
+}
+
+func Connect(cfg *Config) (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
+		Username: cfg.Username,
+		Password: cfg.Password,
+		DB:       cfg.DB,
+	})
+
+	err := client.Ping(context.Background()).Err()
+	if err != nil {
+		return nil, fmt.Errorf("redis.Connect - client.Ping: %w", err)
+	}
+
+	return client, nil
+}
+
+func MustConnect(cfg *Config) *redis.Client {
+	client, err := Connect(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return client
 }
