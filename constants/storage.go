5e4d4890b350c46f47711087fe30a9b8119ac9dc
d1rtyloudx <ilia.mesha166@gmail.com>
2025-02-27 08:50:09 +0300

    add storage constants

--- constants/storage.go
+++ constants/storage.go
@@ -1,1 +1,12 @@
 package constants
+
+import "errors"
+
+var (
+	ErrNotFound      = errors.New("not found")
+	ErrAlreadyExists = errors.New("already exists")
+)
+
+const (
+	UniqueViolationCode = "23505"
+)
